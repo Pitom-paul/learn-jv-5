72045361f962331df39de1cc68974214bd5cac49
Pitom-paul <pitomp185@gmail.com>
2026-07-22 21:38:48 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,14 @@
+// function sayHello(name){
+//     console.log( 'hello! to bhai', name );
+//     console.log(name);
+    
+    
+// } 
+// sayHello('shaku')
+function num(a, b, c, d){
+     const total = a + b + c +d;
+     console.log('sum o 4num', total);
+     
+    
+}
+num(52, 65, 23, 96)
---
+++ return.js
@@ -0,0 +1,8 @@
+function cuthalf(number){
+    const half = number /2;
+    return half;
+}
+console.log('~~~~~~~~~~~~~~~~');
+
+const result = cuthalf(100);
+console.log('value of result', result);
